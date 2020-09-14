c771cf5b368447016d9c0a4fa0606f11da22709d
Myeongseok Kim <msnodeve@gmail.com>
2020-09-14 19:43:20 +0900

    Feat: 런처 페이지 구현
    - Count 넘버링 구현
    - 슬라이드 구현
    - 목차 버튼 구현

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,64 +1,93 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        ZIP-Children
-      </h1>
-      <div class="links">
-        <nuxt-link
-          to="/home"
-          class="button--green"
-        >
-          Home page
-        </nuxt-link>
-      </div>
+  <div class="container-fluid">
+    <div class="launcher_index">
+      <v-ul class="main-right-nav">
+        <li class="item item-main" data-scroll="main-banner">
+          <v-tab @click="goto('#infomation')">
+            <span style="font-size:16px">Infomation</span>
+          </v-tab>
+        </li>
+        <li class="item item-main" data-scroll="main-banner">
+          <v-tab @click="goto('#helped2')">
+            <span style="font-size:16px">UsePlatform</span>
+          </v-tab>
+        </li>
+        <li class="item item-main" data-scroll="main-banner">
+          <v-tab @click="goto('#helped3')">
+            <span style="font-size:16px">Title3</span>
+          </v-tab>
+        </li>
+        <li class="item item-main" data-scroll="main-banner">
+          <v-tab @click="goto('#helped4')">
+            <span style="font-size:16px">Title4</span>
+          </v-tab>
+        </li>
+        <li class="item item-main" data-scroll="main-banner">
+          <v-tab @click="goto('#helped5')">
+            <span style="font-size:16px">Title5</span>
+          </v-tab>
+        </li>
+      </v-ul>
+    </div>
+    <div class="launcher_item">
+      <v-flex>
+        <div id="infomation">
+          <Infomation />
+        </div>
+        <div id="helped2">
+          <UsePlatform />
+        </div>
+        <div id="helped3">
+          <title3 />
+        </div>
+        <div id="helped4">
+          <title4 />
+        </div>
+        <div id="helped5">
+          <title5 />
+        </div>
+      </v-flex>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+
+import Infomation from '@/components/Launcher/Infomation.vue'
+import UsePlatform from '@/components/Launcher/UsePlatform.vue'
+import Title3 from '@/components/Launcher/Title3.vue'
+import Title4 from '@/components/Launcher/Title4.vue'
+import Title5 from '@/components/Launcher/Title5.vue'
+
+export default {
+  components: { Infomation, UsePlatform, Title3, Title4, Title5 },
+  methods: {
+    goto (target) {
+      this.$vuetify.goTo(target, {
+        duration: 300,
+        offset: 100,
+        easing: 'easeInOutCubic'
+      })
+    }
+  }
+}
 </script>
 
-<style scoped>
+<style>
 .container {
   margin: 0 auto;
   min-height: 100vh;
-  width: 100vw;
   display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
 }
 
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+.launcher_index{
+  display: inline-block;
+  position: fixed;
+  z-index: 100;
+  top: 40%;
+  right: 0%;
 }
 </style>
