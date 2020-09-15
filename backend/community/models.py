824c145d8da2688a5ca22584167bfcd27b73ef0c
yeseul4072 <yeseul4072@gmail.com>
2020-09-15 17:54:05 +0900

    ﻿Feat: Community 개발 | S03P22A111-61
        - 들여쓰기 수정

--- backend/community/models.py
+++ backend/community/models.py
@@ -3,30 +3,30 @@ from django.conf import settings
 
 # Create your models here.
 class Board(models.Model):
-  	title = models.CharField(max_length=50)
-  	user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-  	favorite_users = models.ManyToManyField(settings.AUTH_USER_MODEL, through='Favorite', related_name='favorite_boards')
+    title = models.CharField(max_length=50)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    favorite_users = models.ManyToManyField(settings.AUTH_USER_MODEL, through='Favorite', related_name='favorite_boards')
 
 
 class Favorite(models.Model):
-	created_at = models.DateField(auto_now_add=True)
-	user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-	board = models.ForeignKey(Board, on_delete=models.CASCADE)
+    created_at = models.DateField(auto_now_add=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    board = models.ForeignKey(Board, on_delete=models.CASCADE)
 
 
 class Article(models.Model):
-	title = models.CharField(max_length=50)
-	content = models.TextField()
-	created_at = models.DateField(auto_now_add=True)
-	updated_at = models.DateField(auto_now=True)
-	user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-	board = models.ForeignKey(Board, on_delete=models.CASCADE)
-	like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_articles')
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    board = models.ForeignKey(Board, on_delete=models.CASCADE)
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_articles')
 
 
 class Comment(models.Model):
-	content = models.TextField()
-	created_at = models.DateField(auto_now_add=True)
-	updated_at = models.DateField(auto_now=True)
-	article = models.ForeignKey(Article, on_delete=models.CASCADE)
-	user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
--- backend/community/serializers.py
+++ backend/community/serializers.py
@@ -27,33 +27,33 @@ class ArticleSerializer(serializers.ModelSerializer):
 
 
 class ArticleCreateSerializer(serializers.ModelSerializer):
-	user = UserSerializer(required=False)
-	board = BoardSerializer(required=False)
-	class Meta:
-		model = Article
-		fields = ['title', 'content', 'user', 'board']
+    user = UserSerializer(required=False)
+    board = BoardSerializer(required=False)
+    class Meta:
+        model = Article
+        fields = ['title', 'content', 'user', 'board']
 
 
 class ArticleCommentSerializer(serializers.ModelSerializer):
-	user = UserListSerializer(required=True)
-	class Meta:
-		model = Comment
-		fields = ['id', 'content', 'created_at', 'updated_at', 'user']
+    user = UserListSerializer(required=True)
+    class Meta:
+        model = Comment
+        fields = ['id', 'content', 'created_at', 'updated_at', 'user']
 
 
 class ArticleDetailSerializer(serializers.ModelSerializer):
-	comment_set = ArticleCommentSerializer(many=True)
-	like_users_count = serializers.SerializerMethodField()
-	def get_like_users_count(self, obj):
-		return obj.like_users.count()
-	class Meta:
-		model = Article
-		fields = ['id','title', 'content', 'created_at', 'updated_at', 'like_users_count', 'comment_set']
+    comment_set = ArticleCommentSerializer(many=True)
+    like_users_count = serializers.SerializerMethodField()
+    def get_like_users_count(self, obj):
+        return obj.like_users.count()
+    class Meta:
+        model = Article
+        fields = ['id','title', 'content', 'created_at', 'updated_at', 'like_users_count', 'comment_set']
 
 
 class CommentCreateSerializer(serializers.ModelSerializer):
-	user = UserSerializer(required=False)
-	article = ArticleSerializer(required=False)
-	class Meta:
-		model = Comment
-		fields = '__all__'
+    user = UserSerializer(required=False)
+    article = ArticleSerializer(required=False)
+    class Meta:
+        model = Comment
+        fields = '__all__'
