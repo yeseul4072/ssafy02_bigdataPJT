59b2e1345af1cfb727c5ce212e8c1af6b5dddf89
Myeongseok Kim <msnodeve@gmail.com>
2020-09-18 16:38:40 +0900

    Feat: Scroll Up 플로팅 버튼 모듈화 작업
    - <ScrollUp/> 이라는 자식컴포넌트 사용하면 됨

---
+++ frontend/components/Common/Utils/ScrollUp.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="index-floting-button">
+    <transition name="fade">
+      <v-btn
+        v-if="hidden"
+        absolute
+        dark
+        fab
+        top
+        right
+        color="pink"
+        @click="goto(componentName);"
+      >
+        <v-icon>mdi-arrow-up</v-icon>
+      </v-btn>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['componentName'],
+  data () {
+    return {
+      hidden: false
+    }
+  },
+  mounted () {
+    document.addEventListener('scroll', this.handleScroll)
+  },
+  methods: {
+    goto (target) {
+      this.$vuetify.goTo('#we', {
+        duration: 1000,
+        offset: 100,
+        easing: 'easeInOutCubic'
+      })
+    },
+    handleScroll () {
+      this.hidden = window.scrollY !== 0
+    }
+  }
+}
+</script>
+
+<style scoped>
+.index-floting-button{
+  display: inline-block;
+  position: fixed !important;
+  z-index: 100;
+  top: 95%;
+  right: 0%;
+}
+</style>
