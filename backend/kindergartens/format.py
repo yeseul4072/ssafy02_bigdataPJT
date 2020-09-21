3432c72aae833f397e419ec03b1bfa2750f733fb
yeseul4072 <yeseul4072@gmail.com>
2020-09-21 23:35:53 +0900

    ﻿Feat: 추천 알고리즘 | S03P23A111-12
    - kindergartens app 만들기
    - data load를 위해 어린이집 data format 변경

---
+++ backend/kindergartens/format.py
@@ -0,0 +1,19 @@
+import json
+
+jsondata = []
+with open('kindergartens/data2.json', 'r') as json_file:
+    kindergartens =  json.load(json_file)
+
+for kindergarten in kindergartens:
+    temp = {}
+    temp['model'] = 'kindergartens.kindergarten'
+    field = {}
+    for key, value in kindergarten.items():
+        if key == 'after-school_inclusion':
+            key = 'after_school_inclusion'
+        field[key] = value
+        temp['fields'] = field
+    jsondata.append(temp)
+
+with open('data.json', 'w', encoding="UTF-8") as make_file:
+    json.dump(jsondata, make_file, indent="\t")
