9af9526b9c63ab5e693d88b2a6865a2b31522adc
서지훈 <sjh7141@naver.com>
2020-09-23 01:43:57 +0900

    Feat: 어린이집 조회 페이지 구현
    - PieChart 구현
    - PieChart script 로직 추가
    - 어울리는 색조합으로 변경 필요

--- frontend/pages/kinder/_id.vue
+++ frontend/pages/kinder/_id.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="KinderWrapper">
     <v-container fluid>
-      <v-row no-gutters style="height:150vh;margin:40px 0px;">
+      <v-row no-gutters style="height:300vh;margin:40px 0px;">
         <!-- 좌측 -->
         <v-col cols="3" style="padding-right:30px;">
           <v-row style="width:100%;">
@@ -13,7 +13,7 @@
                 :src="require('@/assets/lion.jpg')"
               />
               <div style="text-align:center;font-size:1.5vw;font-weight:800;">
-                {{ info.name }}
+                {{ info.organizationName }}
               </div>
               <div style="text-align:center;">
                 {{ info.address }}
@@ -65,35 +65,113 @@
         </v-col>
         <!-- 우측 -->
         <v-col cols="9">
-          <v-row style="height:30%">
+          <v-row>
             <v-col cols="12" class="cardRight">
-              <v-row style="height:15%">
+              <v-row>
                 <div style="font-size:2.0vw;font-weight:800;">
                   어린이집 소개
                 </div>
               </v-row>
-              <v-row style="height:85%">
-                <kinder-chart title="교사대 아동비율" :number="[info.nteacher, info.nchildren]" :name="['교사','아동']" :color="color1" />
+              <v-row>
+                <v-col cols="12">
+                  내용
+                </v-col>
+                <v-col cols="12">
+                  내용
+                </v-col>
+                <v-col cols="12">
+                  내용
+                </v-col>
+                <v-col cols="12">
+                  내용
+                </v-col>
+                <v-col cols="12">
+                  내용
+                </v-col>
+                <v-col cols="12">
+                  내용
+                </v-col>
+                <v-col cols="12">
+                  내용
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col cols="4">
+                  <kinder-chart
+                    title="교사대 아동비율"
+                    :number="[info.nteacher, info.nchildren]"
+                    :name="['교사','아동']"
+                    :color="color1"
+                    :unit="unit1"
+                    style="height:100%"
+                  />
+                </v-col>
+                <v-col cols="4">
+                  <kinder-chart
+                    title="교사 자격"
+                    :number="[Math.round(100 * info.nOne / (info.nOne + info.nTwo + info.nThree)),Math.round(100 * info.nTwo / (info.nOne + info.nTwo + info.nThree)),Math.round(100 * info.nThree / (info.nOne + info.nTwo + info.nThree))]"
+                    :name="['1급','2급','3급']"
+                    :color="color2"
+                    :unit="unit2"
+                    style="height:100%"
+                  />
+                </v-col>
+                <v-col cols="4">
+                  <kinder-chart
+                    title="근속 년수"
+                    :number="info.yearInfo"
+                    :name="['1년미만', '1년이상 2년미만', '2년이상 4년미만', '4년이상 6년미만', '6년이상'] "
+                    :color="color3"
+                    :unit="unit2"
+                    style="height:100%"
+                  />
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col cols="12">
+                  <div style="width:100%;text-align:right">
+                    <v-btn icon>
+                      <v-icon>fas fa-angle-left</v-icon>
+                    </v-btn>
+                    <v-btn icon>
+                      <v-icon>fas fa-angle-right</v-icon>
+                    </v-btn>
+                  </div>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col cols="4">
+                  교사평균 근속연수
+                  <bar-chart />
+                </v-col>
+                <v-col cols="4">
+                  교사 1인당 유아 수
+                  <bar-chart />
+                </v-col>
+                <v-col cols="4">
+                  100m²당 CCTV수
+                  <bar-chart />
+                </v-col>
               </v-row>
             </v-col>
           </v-row>
           <v-row style="height:3.3%" />
-          <v-row style="height:30%">
+          <v-row style="height:20%">
             <v-col cols="12" class="cardRight">
-              <v-row style="height:15%;">
+              <v-row style="height:10%;">
                 <div style="font-size:2.0vw;font-weight:800;">
                   주변정보
                 </div>
               </v-row>
-              <v-row style="height:85%;">
+              <v-row style="height:90%;">
                 <map-view />
               </v-row>
             </v-col>
           </v-row>
           <v-row style="height:3.3%" />
-          <v-row style="height:30%">
+          <v-row style="height:40%">
             <v-col cols="12" class="cardRight">
-              <v-row style="height:15%;">
+              <v-row style="height:10%;">
                 <div style="font-size:2.0vw;font-weight:800;">
                   <span style="color:#F5A9E1;">"ZIP"</span>&nbsp;리뷰
                 </div>
@@ -112,17 +190,19 @@
 <script>
 import MapView from '@/components/Kinder/Map.vue'
 import KinderChart from '@/components/Kinder/PieChart.vue'
+import BarChart from '@/components/Kinder/BarChart.vue'
 export default {
   components: {
     MapView,
-    KinderChart
+    KinderChart,
+    BarChart
 
   },
   data () {
     return {
       id: this.$route.params.id,
       info: {
-        name: '대봉어린이집',
+        organizationName: '대봉어린이집', // 기관명
         address: '서울특별시 강남구 테헤란로 212 (역삼동 718-5번지)',
         stars: 4.3,
         tags: ['국공립', '야간연장', '방과후 통합', ' 안전교육', '통학차량', 'CCTV 운영'],
@@ -131,10 +211,22 @@ export default {
         nOne: 20,
         nTwo: 15,
         nThree: 11,
-        yearInfo: [21.4, 28.6, 50, 0, 0]
+        yearInfo: [21.4, 28.6, 50, 0, 0],
+        director_name: '윤혜경', // 원장명
+        establishment_type: '직장', // 설립유형
+        created_date: '2014-01-29', // 설립(개원)일
+        tel: '(02)3789-5971',
+        homepage: 'http://www.puruni.com/gsconst',
+        operating_time: '평일기준시간 : 07:30 ~ 19:30',
+        services: ['일반'], // 제공 서비스 [일반 / 영아전담 / 장애아전문 / 장애아통합 / 방과후 전담 / 방과후 통합 / 야간연장 / 휴일보육 / 24시간 / 시간제보육]
+        school_bus: '미운영', // 통학 버스 운영여부 (운영 또는 미운영)
+        grade: 'A' // 어린이집의 등급 또는 종합 평점
       },
-      color1: ['#e25668', '#e2cf56', '#69e256', '#56e2cf', '#5669e2', '#e256ae']
-
+      color1: ['#e25668', '#e2cf56', '#69e256', '#56e2cf', '#5669e2', '#e256ae'],
+      color2: ['#69e256', '#56e2cf', '#5669e2', '#e256ae', '#e25668', '#e2cf56'],
+      color3: ['#5669e2', '#e256ae', '#e25668', '#69e256', '#56e2cf', '#e2cf56'],
+      unit1: '명',
+      unit2: '%'
     }
   },
   method: {
