dce438318e2ef872d9d94895b0e65ba85dc32147
서지훈 <sjh7141@naver.com>
2020-09-24 02:54:05 +0900

    Feat: 어린이집 조회 페이지 구현
    - BarChartVertical 추가
    - 폰트 수정

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -56,7 +56,7 @@ export default {
   buildModules: [
     '@nuxtjs/vuetify',
     // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module',
+    '@nuxtjs/eslint-module'
   ],
   vuetify: {
     /* module options */
