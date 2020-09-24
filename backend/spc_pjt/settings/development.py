abdd8cb2a5f14c82fb3aeba7654b001396a984ac
YongjoonSeo <koreakkrea12@naver.com>
2020-09-24 15:56:46 +0900

    Feat: 개발 로그 매터모스트 전송 기능 | S03P23A111-35
    - 개발 서버의 서버에러 매터모스트로 전송
    - secrets-dev.json에 매터모스트 url 작성 필요

--- backend/spc_pjt/settings/development.py
+++ backend/spc_pjt/settings/development.py
@@ -1,6 +1,8 @@
 from .base import *
 import os
 from datetime import date
+import logging
+import requests
 
 
 DEBUG = True
@@ -12,8 +14,18 @@ secrets = Secrets(secret_file)
 
 DATABASES = secrets.get_secret('DATABASES')
 
+class MattermostRequestsHandler(logging.Handler):
+        
+    def emit(self, record):
+        log_entry = self.format(record)
+        return requests.post(
+            url=secrets.get_secret('MATTERMOST_URL'),
+            data=log_entry.encode('utf-8'),
+            headers={'Content-type': 'application/json'}
+        )
+
 log_file_path = os.path.join(os.path.dirname(os.path.dirname(BASE_DIR)), 'django-dev-log')
-log_file_name = f'{date.today().isoformat()}-django.log'
+log_file_name = f'django.log.{date.today().isoformat()}'
 
 LOGGING = {
     'version': 1,
@@ -22,18 +34,28 @@ LOGGING = {
         'standard': {
             'format': '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
         },
+        'json': {
+            'format': '{"text": "%(asctime)s %(name)s %(levelname)s %(message)s"}'
+        }
     },
     'handlers': {
         'file': {  
-            'class': 'logging.FileHandler',
-            'filename': os.path.join(log_file_path, log_file_name),  
-            'formatter': 'standard'
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'filename': os.path.join(log_file_path, log_file_name),
+            'when': 'midnight',
+            'backupCount': 100,
+            'formatter': 'standard',
         },
+        'http': {
+            'class': 'spc_pjt.settings.development.MattermostRequestsHandler',
+            'formatter': 'json'
+        }
     },
     'loggers': {
         'django': {
-            'handlers': ['file'],  
+            'handlers': ['file', 'http'],  
             'level': os.getenv('DJANGO_LOG_LEVEL', 'ERROR'),
+            'propagate': False
         },
     },
 }
