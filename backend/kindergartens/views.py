5e757a7f3ef1c2d75444ef3b43036848bbe8bc32
YongjoonSeo <koreakkrea12@naver.com>
2020-09-27 17:03:01 +0900

    Chore: recommend path append
    - sys.path.append 적용
    - force push 테스트
    - docker-compose --force-recreate --renew-anon-volumes 옵션

--- backend/kindergartens/views.py
+++ backend/kindergartens/views.py
@@ -18,7 +18,7 @@ import pandas as pd
 # recommendation algorithm
 import os
 import sys
-# sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))))
+sys.path.append(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__)))))
 from bigdata import recommend
 from haversine import haversine
 
