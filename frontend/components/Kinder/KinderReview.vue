469963e37bfc9702dcab3a91d5ba62f9bfa4c5b6
서지훈 <sjh7141@naver.com>
2020-09-28 11:17:59 +0900

    Fix: 어린이집 조회 페이지 수정
    - 반응형 화면 버그 수정

--- frontend/components/Kinder/KinderReview.vue
+++ frontend/components/Kinder/KinderReview.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
-    <v-row style="border:thin solid #E6E6E6;">
+    <v-row style="border:thin solid #E6E6E6; border-radius:15px;">
       <v-col cols="3" align-self="center">
         <v-row justify="center">
           <v-col cols="12" align="center">
-            <v-avatar size="18vh">
+            <v-avatar size="12vw">
               <v-img
                 :src="'https://www.iconfinder.com/data/icons/female-avatars-vol-1/256/female-portrait-avatar-profile-woman-sexy-afro-2-512.png'"
+                height="150px"
+                width="150px"
               />
             </v-avatar>
           </v-col>
@@ -82,39 +84,39 @@
           </v-col>
         </v-row>
       </v-col>
-      <v-col cols="9" style="padding-top:50px;padding-bottom:50px;">
-        <v-row style="padding:10px;border-left:thin solid #E6E6E6;">
+      <v-col cols="9" class="py-7">
+        <v-row class="pa-3" style="border-left:thin solid #E6E6E6;">
           <v-row style="padding-left:16px;font-size:2vh;font-weight:800;width:100%;">
             "{{ review.title }}""
           </v-row>
-          <v-row style="padding-left:16px;font-size:1.5vh;width:100%;">
+          <v-row class="pl-4" style="font-size:1.5vh;width:100%;">
             <div style="display:inline-block" class="pr-3">
               unseng@gmail.com
             </div>
-            <i style="color:#DEDEDE" class="fas fas fa-ellipsis-v" />
+            <i class="fas fas fa-ellipsis-v" style="color:#DEDEDE" />
             <div style="display:inline-block" class="pl-3">
               {{ '2020.09.25' | diffDate }}
             </div>
           </v-row>
           <v-row>
-            <v-card-title style="height:10%">
+            <v-card-title class="px-4 pt-2 pb-1">
               <v-chip class="success white--text">
                 장점
               </v-chip>
             </v-card-title>
 
-            <v-card-text style="height:13vh;overflow:hidden;">
+            <v-card-text style="overflow:hidden;">
               <!-- https://www.npmjs.com/package/vue-line-clamp -->
               <div v-html="review.pros.replace(/(?:\r\n|\r|\n)/g, '<br />')" />
             </v-card-text>
             <v-divider />
-            <v-card-title style="height:10%">
+            <v-card-title class="px-4 pt-2 pb-1">
               <v-chip class="pink white--text">
                 단점
               </v-chip>
             </v-card-title>
 
-            <v-card-text style="height:13vh">
+            <v-card-text style="overflow:hidden;">
               <div v-html="review.cons.replace(/(?:\r\n|\r|\n)/g, '<br />')" />
             </v-card-text>
           </v-row>
@@ -146,17 +148,9 @@ export default {
       return `by ${val}`
     }
   },
+  props: ['review'],
   data () {
     return {
-      review: {
-        name: '대봉어린이집',
-        tags: ['국공립', '야간연장', '방과후 통합', ' 안전교육', '통학차량', 'CCTV 운영'],
-        url: 'kinder_temp.jpg',
-        title: '믿고 맡길 수 있는 어린이집',
-        stars: 5,
-        pros: '1원장선생님이 교육에 대한 열의가 굉장하시다고 느꼈다. \n원비 대비 하는 활동이나 교육프로그램이 많고 다양해서 아이가 항상 유치원 가기를 즐거워한다. 또한 현장학습이나 생일파티때 따로 음식을 준비하지 않아도 원에서 준비해 주신다. \n매주 학습이 어떻게 이루어졌는지 그주의 활동을 정리해 보내주신다.',
-        cons: '1원장선생님이 교육에 대한 열의가 굉장하시다고 느꼈다. \n원비 대비 하는 활동이나 교육프로그램이 많고 다양해서 아이가 항상 유치원 가기를 즐거워한다. 또한 현장학습이나 생일파티때 따로 음식을 준비하지 않아도 원에서 준비해 주신다. \n매주 학습이 어떻게 이루어졌는지 그주의 활동을 정리해 보내주신다.'
-      }
     }
   }
 }
