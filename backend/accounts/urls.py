ba6eaa0913163ddbbab116e08ac130b5cd02c080
YongjoonSeo <koreakkrea12@naver.com>
2020-10-05 19:15:35 +0900

    Fix: 소셜로그인 보류
    - 소셜로그인 작성 부분 주석처리

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
   path('delete/', views.user_delete, name='user_delete'), 
   path('user/profile/', views.user_profile, name='user_profile'),
-  path('kakao/login/', views.kakao_login, name='kakao_login'),
-  path('kakao/login/callback/', views.kakao_callback, name='kakao_callback'),
+  # path('kakao/login/', views.kakao_login, name='kakao_login'),
+  # path('kakao/login/callback/', views.kakao_callback, name='kakao_callback'),
 ]
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import redirect, get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view, permission_classes
 from django.http import HttpResponse
-from spc_pjt.settings.base import Secrets, BASE_DIR
+# from spc_pjt.settings.base import Secrets, BASE_DIR
 from django.contrib.auth import get_user_model
 from .serializers import UserSerializer
 from rest_framework.response import Response
@@ -14,8 +14,8 @@ from drf_yasg.utils import swagger_auto_schema
 from . import request_schemas
 
 
-secret_file = os.path.join(BASE_DIR, 'secrets-local.json')
-secrets = Secrets(secret_file)
+# secret_file = os.path.join(BASE_DIR, 'secrets-local.json')
+# secrets = Secrets(secret_file)
 
 User = get_user_model()
 
@@ -48,36 +48,36 @@ def user_profile(request):
     serializer = UserSerializer(user)
     return Response(serializer.data)
 
-# 카카오 소셜로그인
-kakao_secret = secrets.get_secret('KAKAO')
-REST_API_KEY = kakao_secret['REST_API_KEY']
-REDIRECT_URI = kakao_secret['REDIRECT_URI']
+# # 카카오 소셜로그인
+# kakao_secret = secrets.get_secret('KAKAO')
+# REST_API_KEY = kakao_secret['REST_API_KEY']
+# REDIRECT_URI = kakao_secret['REDIRECT_URI']
 
-class KakaoException(Exception):
-    pass
+# class KakaoException(Exception):
+#     pass
 
-# 인증 코드 요청
-def kakao_login(request):
-    kakao_code_request_url = f'https://kauth.kakao.com/oauth/authorize?client_id={REST_API_KEY}&redirect_uri={REDIRECT_URI}&response_type=code'
-    return redirect(kakao_code_request_url)
+# # 인증 코드 요청
+# def kakao_login(request):
+#     kakao_code_request_url = f'https://kauth.kakao.com/oauth/authorize?client_id={REST_API_KEY}&redirect_uri={REDIRECT_URI}&response_type=code'
+#     return redirect(kakao_code_request_url)
 
-def kakao_callback(request):
-    # try:
-    AUTHORIZE_CODE = request.GET.get('code')
-    kakao_token_request_url = f'https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id={REST_API_KEY}&redirect_uri={REDIRECT_URI}&code={AUTHORIZE_CODE}'
+# def kakao_callback(request):
+#     # try:
+#     AUTHORIZE_CODE = request.GET.get('code')
+#     kakao_token_request_url = f'https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id={REST_API_KEY}&redirect_uri={REDIRECT_URI}&code={AUTHORIZE_CODE}'
     
-    # 인증 코드로 토큰 요청
-    token_request = requests.get(kakao_token_request_url)
-    token_response_json = token_request.json()
-    error = token_response_json.get('error', None)
+#     # 인증 코드로 토큰 요청
+#     token_request = requests.get(kakao_token_request_url)
+#     token_response_json = token_request.json()
+#     error = token_response_json.get('error', None)
 
-    if error is not None:
-        raise KakaoException()
-    USER_ACCESS_TOKEN = token_response_json.get('access_token')
+#     if error is not None:
+#         raise KakaoException()
+#     USER_ACCESS_TOKEN = token_response_json.get('access_token')
 
-    # 토큰으로 사용자 정보 API 호출
-    headers = {'Authorization': f'Bearer {USER_ACCESS_TOKEN}'}
-    profile_request = requests.post('https://kapi.kakao.com/v2/user/me', headers=headers)
-    profile_json = profile_request.json()
-    print(profile_json)
-    return redirect('http://childrenzip.site')
+#     # 토큰으로 사용자 정보 API 호출
+#     headers = {'Authorization': f'Bearer {USER_ACCESS_TOKEN}'}
+#     profile_request = requests.post('https://kapi.kakao.com/v2/user/me', headers=headers)
+#     profile_json = profile_request.json()
+#     print(profile_json)
+#     return redirect('http://childrenzip.site')
