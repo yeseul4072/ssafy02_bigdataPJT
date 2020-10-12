91a8ab140a00217ee67c2b90fb50fccb6ba0751e
YongjoonSeo <koreakkrea12@naver.com>
2020-10-13 03:03:11 +0900

    Chore: kakao 및 요청 https로 변경

--- frontend/pages/signup/kindergarden.vue
+++ frontend/pages/signup/kindergarden.vue
@@ -160,7 +160,8 @@
 </template>
 
 <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>
-<script type="text/JavaScript" src="http://dmaps.daum.net/map_js_init/postcode.v2.js"></script>
+<!--<script type="text/JavaScript" src="http://dmaps.daum.net/map_js_init/postcode.v2.js"></script> -->
+<script type="text/JavaScript" src="https://ssl.daumcdn.net/dmaps/map_js_init/postcode.v2.js"></script>
 <script>
 
 export default {
